af4f297d1dc835cd430fc73d2983c46329af5d3f
ryuseongryong <ryuseongryong@gmail.com>
2021-08-10 08:56:11 +0900

    Loca-Musica-Add-서버 초기 개발 환경 구축

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MusicalModule } from './musical/musical.module';
+import { SearchModule } from './search/search.module';
+import { AdminModule } from './admin/admin.module';
+import { RouterModule } from '@nestjs/core';
+import { OauthModule } from './oauth/oauth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, MusicalModule, SearchModule, AdminModule, OauthModule],
   controllers: [AppController],
   providers: [AppService],
 })
