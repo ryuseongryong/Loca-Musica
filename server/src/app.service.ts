98f9d5f6daf66af2d02d18ad6df67c81f2261f0a
ryuseongryong <ryuseongryong@gmail.com>
2021-08-10 16:54:38 +0900

    app.service.ts-Fix-배포테스트

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Loca-Musica!';
+    return 'Hello Loca-Musica! 210810!!';
   }
 }
