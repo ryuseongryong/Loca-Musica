2f80e7f26d63555dbf771679c77e27f808cd0877
ryuseongryong <ryuseongryong@gmail.com>
2021-08-10 17:07:21 +0900

    stop.sh-Fix-배포테스트2

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/loca-musica/server/src
-pm2 stop main.ts 2> /dev/null || true
-pm2 delete main.ts 2> /dev/null || true
+cd /home/ubuntu/loca-musica/server/dist
+pm2 stop main.js 2> /dev/null || true
+pm2 delete main.js 2> /dev/null || true
