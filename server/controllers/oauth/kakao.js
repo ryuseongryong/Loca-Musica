1a361a244b355de596c62e8865a6af3d50541e44
jhoryong <jhoryong@gmail.com>
2021-08-13 17:52:11 +0900

    kakao.js-fix-add dotenv variable

--- server/controllers/oauth/kakao.js
+++ server/controllers/oauth/kakao.js
@@ -14,6 +14,7 @@ module.exports = {
         'client_id:', process.env.KAKAO_CLIENT_ID,
         'redirect_uri:', process.env.KAKAO_REDIRECT_URI,
       )
+      // asdfasdf
       axios({
         method:'post',
         url:'https://kauth.kakao.com/oauth/token',
