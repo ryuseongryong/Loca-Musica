b8f1c6711e6f1f337f11e8131916feb313cb80d3
ryuseongryong <ryuseongryong@gmail.com>
2021-08-13 15:47:24 +0900

    signin, signout, signup, auth-Add-Local user인증 구현

--- server/controllers/user/signin.js
+++ server/controllers/user/signin.js
@@ -1,3 +1,64 @@
+const db = require('../../db');
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  sendRefreshToken,
+  sendAccessToken,
+  checkAccessToken,
+} = require('../tokenFunctions');
+
 module.exports = {
-  post: async (req, res) => {},
+  post: async (req, res) => {
+    const { email, password } = req.body;
+
+    const connection1 = await db.getConnection(async (conn) => conn);
+    const connection2 = await db.getConnection(async (conn) => conn);
+    const connection3 = await db.getConnection(async (conn) => conn);
+
+    connection1.beginTransaction();
+    connection2.beginTransaction();
+    connection3.beginTransaction();
+
+    // 이메일과 비밀번호를
+    let [userData] = await connection1.query(
+      `SELECT * FROM users WHERE email = "${email}"`
+    );
+    connection1.commit();
+    connection1.destroy();
+
+    if (userData.length === 0) {
+      res.status(404).send({ message: 'invalid email' });
+    } else if (userData[0].password !== password) {
+      res.status(404).send({ message: 'invalid password' });
+    } else if (userData.length !== 0 && userData[0].password === password) {
+      // Access Token 발급
+      const { id, email, username, profile, resign, admin } = userData[0];
+
+      const accessToken = generateAccessToken({
+        id,
+        username,
+        email,
+        profile,
+        resign,
+        admin,
+      });
+
+      const refreshToken = generateRefreshToken({
+        id,
+        username,
+        email,
+        profile,
+        resign,
+        admin,
+      });
+
+      // send Token
+      sendAccessToken(res, accessToken);
+      sendRefreshToken(res, refreshToken);
+
+      res.status(200).json({ data: userData[0], message: 'ok' });
+    } else {
+      res.status(500).send({ message: 'internal server error' });
+    }
+  },
 };
--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -1,7 +1,83 @@
 const db = require('../../db');
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  sendRefreshToken,
+  sendAccessToken,
+  checkAccessToken,
+} = require('../tokenFunctions');
 
 module.exports = {
   post: async (req, res) => {
-    res.send('signup');
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      res.status(422).send({ message: 'input empty' });
+    }
+
+    try {
+      console.log(req.body);
+      const connection1 = await db.getConnection(async (conn) => conn);
+      const connection2 = await db.getConnection(async (conn) => conn);
+      const connection3 = await db.getConnection(async (conn) => conn);
+
+      connection1.beginTransaction();
+      connection2.beginTransaction();
+      connection3.beginTransaction();
+
+      // 이메일과 비밀번호를
+      let [userData] = await connection1.query(
+        `SELECT * FROM users WHERE email = "${email}"`
+      );
+      connection1.commit();
+      connection1.destroy();
+
+      if (userData[0]) {
+        res.status(409).send({ message: 'email conflict' });
+      } else {
+        const [create] = await connection2.query(
+          `INSERT INTO users (email, password, username) VALUES (?, ?, ?)`,
+          [email, password, username]
+        );
+        connection2.commit();
+        connection2.release();
+
+        const [newUserData] = await connection2.query(
+          `SELECT * FROM users WHERE email = "${email}"`
+        );
+        connection2.commit();
+        connection2.destroy();
+
+        const { id, profile, resign, admin } = newUserData[0];
+
+        const accessToken = generateAccessToken({
+          id,
+          username,
+          email,
+          profile,
+          resign,
+          admin,
+        });
+
+        const refreshToken = generateRefreshToken({
+          id,
+          username,
+          email,
+          profile,
+          resign,
+          admin,
+        });
+
+        // send Token
+        sendAccessToken(res, accessToken);
+        sendRefreshToken(res, refreshToken);
+
+        res
+          .status(201)
+          .json({ data: newUserData[0], message: 'signup success' });
+      }
+    } catch (err) {
+      res.status(500).send({ message: 'internal server error' });
+    }
   },
 };
